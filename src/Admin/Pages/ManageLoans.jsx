89a7565d898db83be44c405e591d2d996e4a9c7a
JefryPaul <jefrypaul73@gmail.com>
2025-11-17 21:00:41 +0530

    Admin and User components are created

---
+++ src/Admin/Pages/ManageLoans.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ManageLoans() {
+  return (
+    <div>ManageLoans</div>
+  )
+}
+
+export default ManageLoans
---
+++ src/Admin/Pages/ManageTransactions.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ManageTransactions() {
+  return (
+    <div>ManageTransactions</div>
+  )
+}
+
+export default ManageTransactions
